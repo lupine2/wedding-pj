09965bc10ac27f9fcadfe2111b464fae5fd9bc1a
lupine2 <52910487+lupine2@users.noreply.github.com>
2019-07-23 16:56:45 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// main.js
+$(function(){
+    $('.slider').bxSlider({
+        mode: 'fade',
+        controls: false,
+        pager: false,
+        auto: true,
+        pause: 5000
+    });
+});
